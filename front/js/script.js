541c25f09ebd6faa9dd72d4a6766650b2176e014
Djaouel <djaouel.nahla@hotmail.fr>
2022-06-22 20:36:50 +0200

    first commit

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,8 @@
-alert ( "hello tout le monde")
+fetch ( "http://localhost:3000/api/products")
+   .then ((res) => res.json ())
+   .then ((resp) => console.log (resp))
+
+
+const products =document.createElement("a")
+products.href = "http://localhost:3000/images/kanap01.jpeg"
+products.text = "Kanap Sinopé"
