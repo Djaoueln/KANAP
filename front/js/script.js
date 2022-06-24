cd3274e2a7c506f49adf856abaab4e3064e80bc3
Djaouel <djaouel.nahla@hotmail.fr>
2022-06-24 13:04:25 +0200

    récupération de l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,14 @@
 fetch ( "http://localhost:3000/api/products")
    .then ((res) => res.json ())
-   .then ((resp) => console.log (resp))
+   .then ((resp) => addProducts(resp))
 
+function addProducts(resp) {
 
+for(i = 0; i<8;i++) {resp[i].name}
 const products =document.createElement("a")
 products.href = "http://localhost:3000/images/kanap01.jpeg"
 products.text = "Kanap Sinopé"
+const items = document.querySelector ("#items")
+if (items != null) {
+   items.appendChild (products)
+}
