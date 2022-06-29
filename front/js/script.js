31fea5342918101b81343ec9d7919b25d5fa5382
Djaouel <djaouel.nahla@hotmail.fr>
2022-06-29 16:44:14 +0200

    js

--- front/js/script.js
+++ front/js/script.js
@@ -1,34 +1,38 @@
 fetch ( "http://localhost:3000/api/products")
    .then((res) => res.json())
-   .then((resp) => {
-      console.log(resp)
-      return addProducts(resp)
-   })
+   .then((resp) =>  displayAllProducts(resp))
   
 
-function addProducts(data) {
-   const id = data[0]._id
-   const imageUrl = data[0].imageUrl
-   const altTxt = data[0].altTxt
-   const image = makeImage(imageUrl, altTxt)
+function displayAllProducts(resp) {
+   for(i = 0; i<8;i++) {
+   const {_id, imageUrl, altTxt, name, description} = resp[i]
    
-   const products = makeProducts(id)
-   const article = makeArticle()
-   article.appendChild(image)
-   appendChildren(products, article)
+   const products = makeProducts(_id)
+   const article = document.createElement("article")
+   const image = makeImage(imageUrl, altTxt)
+   const h3 = makeH3(name)
+   const p = makeParagraph(description)
+   
+   ElementsToArticle(article, image, h3, p)
+   ArticleProducts(products, article)
+   }
 }
-
+function ElementsToArticle(article, image, h3, p){
+   article.appendChild(image)
+   article.appendChild(h3)
+   article.appendChild(p)
+}
 
 function makeProducts(id) {
 const products =document.createElement("a")
 products.href = "./product.html?id=" + id
 return products
 }
-function appendChildren(products, article) {
+function ArticleProducts(products, article) {
    const items = document.querySelector ("#items")
    if (items != null) {
       items.appendChild(products)
-      items.appendChild(article)
+      products.appendChild(article)
 }}
 function makeImage(imageUrl, altTxt) {
    const image = document.createElement("img")
@@ -36,23 +40,18 @@ function makeImage(imageUrl, altTxt) {
    image.alt = altTxt
    return image
 }
-function makeArticle() {
-const article = document.createElement("article")
-const h3 =makeH3()
-const paragraph = makeParagraph()
-// article.appendChild(image)
-// article.appendChild(h3)
-// article.appendChild(paragraph)
-console.log(article)
-return article
+
+function makeH3(name) {
+const h3 = document.createElement("h3")
+h3.textContent = name
+h3.classList.add("productName")
+return h3
 }
 
-function makeH3() {
-
+function makeParagraph(description) {
+const p = document.createElement("p")
+p.textContent = description
+p.classList.add("productDescription")
+return p
 }
 
-function makeParagraph() {
-
-}
-
-// for(i = 0; i<8;i++) {resp[i].name}
