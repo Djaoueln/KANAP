e84c7e28b4aa1e47945974ed60c44d2f8bc19166
Djaouel <djaouel.nahla@hotmail.fr>
2022-06-30 16:35:31 +0200

    script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,57 +1,21 @@
 fetch ( "http://localhost:3000/api/products")
    .then((res) => res.json())
-   .then((resp) =>  displayAllProducts(resp))
-  
+   .then((resp) => displayAllProducts(resp))
 
 function displayAllProducts(resp) {
-   for(i = 0; i<8;i++) {
-   const {_id, imageUrl, altTxt, name, description} = resp[i]
-   
-   const products = makeProducts(_id)
-   const article = document.createElement("article")
-   const image = makeImage(imageUrl, altTxt)
-   const h3 = makeH3(name)
-   const p = makeParagraph(description)
-   
-   ElementsToArticle(article, image, h3, p)
-   ArticleProducts(products, article)
+      let items = document.getElementById("items");
+      items.innerHTML = '';  
+   for( let i = 0; i<resp.length;i++) {
+      items.innerHTML += htmlProduct (resp[i]);
    }
 }
-function ElementsToArticle(article, image, h3, p){
-   article.appendChild(image)
-   article.appendChild(h3)
-   article.appendChild(p)
+function htmlProduct(article) {
+let htlmA = '<a href="./product.html?id=' +article._id+ '">';
+htlmA += '<article>';
+htlmA +='<img src=" '+article.imageUrl+' " alt="' +article.altTxt+ '">';
+htlmA += '<h3 class="productName">' +article.name+ '</h3>';
+htlmA += '<p class="productDescription">' +article.description+ '</p>'; 
+htlmA += '</article>'; 
+htlmA += '</a>';
+return htlmA;
 }
-
-function makeProducts(id) {
-const products =document.createElement("a")
-products.href = "./product.html?id=" + id
-return products
-}
-function ArticleProducts(products, article) {
-   const items = document.querySelector ("#items")
-   if (items != null) {
-      items.appendChild(products)
-      products.appendChild(article)
-}}
-function makeImage(imageUrl, altTxt) {
-   const image = document.createElement("img")
-   image.src = imageUrl
-   image.alt = altTxt
-   return image
-}
-
-function makeH3(name) {
-const h3 = document.createElement("h3")
-h3.textContent = name
-h3.classList.add("productName")
-return h3
-}
-
-function makeParagraph(description) {
-const p = document.createElement("p")
-p.textContent = description
-p.classList.add("productDescription")
-return p
-}
-
