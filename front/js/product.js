92fa2275a0ba8e7de9ef0eeb16b5a13fb6b3e2fe
Djaouel <djaouel.nahla@hotmail.fr>
2022-07-07 18:33:40 +0200

    UrlSearch

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,35 @@
-var str = window.location.href;
-var url = new URL(str);
-var search_params = new URLSearchParams(url.search); 
+let str = window.location.href;
+let url = new URL(str);
+let search_params = new URLSearchParams(url.search); 
 if(search_params.has('id')) {
-  var id = search_params.get('id');
-}
-
-for (let p of search_params) 
-  {
-  }
-  
-fetch (`http://localhost:3000/api/products/${id}`)
+  let id = search_params.get('id');
+  fetch (`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((resp) => addProducts(resp))
+}
+
+  
+
 
 
 function addProducts(kanap){
-  const {imageUrl, altTxt, colors} = kanap
-   image(imageUrl, altTxt)
-   document.getElementById("title").innerText = kanap.name;
-   document.getElementById("price").innerText = kanap.price + " ";
-   document.getElementById("description").innerText = kanap.description;
+  const {imageUrl, altTxt, colors} = kanap;
+   setImage(imageUrl, altTxt);
+   setInfo (kanap);
+   setColors (colors);
 
-   colors.forEach((color) => {
+}
+
+function setImage(imageUrl, altTxt){
+ const img = document.createElement("img");
+ img.src = imageUrl
+ img.alt = altTxt
+ document.getElementsByClassName("item__img")[0].appendChild(img);
+}
+
+function setColors (colors)
+{
+  colors.forEach((color) => {
     const option = document.createElement("option")
     const select = document.getElementById("colors");
 
@@ -31,15 +39,12 @@ function addProducts(kanap){
     select.appendChild(option)
    });
 }
-
-function image(imageUrl, altTxt ){
- const img = document.createElement("img");
- img.src = imageUrl
- img.alt = altTxt
- document.getElementsByClassName("item__img")[0].appendChild(img);
+function setInfo (product)
+{
+   document.getElementById("title").innerText = product.name;
+   document.getElementById("price").innerText = product.price + " ";
+   document.getElementById("description").innerText = product.description;
 }
-
-
 
 
 
